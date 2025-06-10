8110cd349457cd4cc63fb563ea15afcc4d57c77f
jasmin-0107 <hxiang63@gmail.com>
2025-06-10 21:43:50 +0200

    Added calculation functions a+b, a-b

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Hello, Python!")
+def Add(a,b):
+    return a + b
+def Sub(a,b):
+    return a - b
+
+def main():
+    print("Hello, Python!")
+    a=5
+    b=3
+    print("a=", a, "b=", b)
+    print("Calculating a+b:", Add(a, b))
+    print("Calculating a-b:", Sub(a, b))
+    print("Calculating a+b, a-b:", Add(a, b), Sub(a, b))
+
+if __name__ == "__main__":
+    main()
