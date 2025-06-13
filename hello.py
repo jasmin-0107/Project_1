88105717ed311a37a7f7f0ee6c1f6925f20ad5f0
jasmin-0107 <hxiang63@gmail.com>
2025-06-13 20:40:05 +0200

    Refactor main function for improved readability and user input handling

--- hello.py
+++ hello.py
@@ -2,15 +2,24 @@ def Add(a,b):
     return a + b
 def Sub(a,b):
     return a - b
+def Mul(a,b):
+    return a * b
+def Div(a,b):
+    if b == 0:
+       return "Error: Division by zero"""
+    return a / b
 
 def main():
     print("Hello, Python!")
-    a=5
-    b=3
+    a=input("input a: ")
+    b=input("input b: ")
+    a = int(a)
+    b = int(b)
     print("a=", a, "b=", b)
     print("Calculating a+b:", Add(a, b))
     print("Calculating a-b:", Sub(a, b))
-    print("Calculating a+b, a-b:", Add(a, b), Sub(a, b))
+    print("Calculating a*b:", Mul(a, b))
+    print("Calculating a/b:", Div(a, b))
 
 if __name__ == "__main__":
     main()
